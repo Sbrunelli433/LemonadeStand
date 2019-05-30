7cb5617723ac8a419200f58938095a7cabfdb976
Sbrunelli433 <brunes46@gmail.com>
2019-05-30 10:32:15 -0500

    added SetCondition method for my weather object. Also created switch case method for adding customers to Rain method in my Day object.

--- Lemonade_Stand_UML/Day.cs
+++ Lemonade_Stand_UML/Day.cs
@@ -8,26 +8,12 @@ namespace Lemonade_Stand_UML
     public class Day
     {
         public int weather;
-        public int lengthOfDay;
         public List<Customer> customers;
-        private object easyCustomer;
-        private object stingyCustomer;
-        private object sourCustomer;
-        private object sweetCustomer;
-        private object coldCustomer;
-        private object difficultCustomer;
-
 
         //public Random number generator
 
         public Day()
         {
-            easyCustomer = new EasyCustomer();
-            stingyCustomer = new StingyCustomer();
-            sourCustomer = new SourCustomer();
-            sweetCustomer = new SweetCustomer();
-            coldCustomer = new ColdCustomer();
-            difficultCustomer = new DifficultCustomer();
 
             customers = new List<Customer>()
             {
@@ -58,48 +44,30 @@ namespace Lemonade_Stand_UML
         }
 
         public void GenerateCustomersForRain()
-            
         {
             Random rng = new Random();
-            int customer = rng.Next(1, 6);
-
-            customers = new List<Customer>();
-
-
-                new EasyCustomer();
-                new StingyCustomer();
-                new SourCustomer();
-                new SweetCustomer();
-                new ColdCustomer();
-                new DifficultCustomer();
-      
+            int customer = rng.Next(1, 6);      
 
             for (int i = 0; i < 50; i++)
             { 
-                switch (i)
+                switch (customer)
                 {
                     case 1:
-                        rng = 1;
-                        new EasyCustomer();
+                        customers.Add(new EasyCustomer());
                         break;
                     case 2:
-                        rng = 2;
                         new StingyCustomer();
                         break;
                     case 3:
-                        rng = 3;
                         new SourCustomer();
                         break;
                     case 4:
-                        rng = 4;
                         new SweetCustomer();
                         break;
                     case 5:
-                        rng = 4;
                         new ColdCustomer();
                         break;
                     case 6:
-                        rng = 5;
                         new DifficultCustomer();
                         break;
                 }
--- Lemonade_Stand_UML/Weather.cs
+++ Lemonade_Stand_UML/Weather.cs
@@ -8,6 +8,7 @@ namespace Lemonade_Stand_UML
     public class Weather
     {
         public List<string> conditions;
+        public string condition;
         public int random;
 
         public Weather()
@@ -16,9 +17,9 @@ namespace Lemonade_Stand_UML
             int temperature = rng.Next(50, 99);
 
 
+           
 
-
-            List<string> conditions = new List<string>();
+            conditions = new List<string>();
             conditions.Add("sunny");
             conditions.Add("cloudy");
             conditions.Add("rainy");
@@ -27,12 +28,17 @@ namespace Lemonade_Stand_UML
             conditions.Add("foggy");
 
 
+            rng.Next(0, conditions.Count);
         }
 
 
-        public void SetForecast()
+        public void SetCondition()
         {
-            throw new System.NotImplementedException();
+            Random rng = new Random();
+            int conditionIndex = rng.Next(0, conditions.Count);
+            condition = conditions[conditionIndex];
+            Console.WriteLine("the weather for this condition is " + condition);
+            Console.ReadLine();
         }
 
         public void Rain()
